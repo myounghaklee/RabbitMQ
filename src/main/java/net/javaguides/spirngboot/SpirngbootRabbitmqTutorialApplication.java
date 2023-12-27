be76d725d1ef41bf7dc3a7b141ba0d2900bff617
Enzo <mhlee@maxst.com>
2023-12-27 15:38:57 +0900

    first commit rabbitmq setting

---
+++ src/main/java/net/javaguides/spirngboot/SpirngbootRabbitmqTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.spirngboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootRabbitmqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootRabbitmqTutorialApplication.class, args);
+	}
+
+}
