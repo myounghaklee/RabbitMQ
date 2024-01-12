07a419d2919816113761e8e7935c577cf9d175fd
Enzo <devhak.enzo@gmail.com>
2024-01-12 14:53:41 +0900

    json message 메세지 전송 method 생성

--- src/main/java/net/javaguides/spirngboot/publisher/RabbitMQJsonProducer.java
+++ src/main/java/net/javaguides/spirngboot/publisher/RabbitMQJsonProducer.java
@@ -3,21 +3,30 @@ package net.javaguides.spirngboot.publisher;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import net.javaguides.spirngboot.dto.User;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RabbitMQJsonProducer {
 
+    @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
+    @Value("${rabbitmq.routing.json.name}")
     private String routingJsonKey;
+
 
     private RabbitTemplate rabbitTemplate;
 
-    private void sendJsonMessage(User user){}
+    private void sendJsonMessage(User user){
+        log.info(String.format("Json message sent ->{}", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
 
 
 }
