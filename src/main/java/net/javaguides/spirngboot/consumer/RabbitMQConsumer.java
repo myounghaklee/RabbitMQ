c43bf2afbc4bdf9b59b354107ca8c92c81ce6e51
Enzo <devhak.enzo@gmail.com>
2024-01-12 13:38:39 +0900

    consummer  생성

--- src/main/java/net/javaguides/spirngboot/consumer/RabbitMQConsumer.java
+++ src/main/java/net/javaguides/spirngboot/consumer/RabbitMQConsumer.java
@@ -1,4 +1,17 @@
 package net.javaguides.spirngboot.consumer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class RabbitMQConsumer {
+
+    @RabbitListener(queues = {"$r{rabbitmq.queue.name"})
+    public void consume(String message){
+        log.info("Received Message -> {}", message);
+
+    }
 }
